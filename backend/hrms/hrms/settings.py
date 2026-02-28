f6a9cd6e37b754eaa1291438a87fd451253e9bb3
Nishant Kumar <nishant.kumar@instantsys.com>
2026-02-28 14:00:53 +0000

    Update settings.py

--- backend/hrms/hrms/settings.py
+++ backend/hrms/hrms/settings.py
@@ -24,9 +24,9 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-n3=tx&*0xiepx7r-t&qdp9k6sd2qm&@e^%r+!nt+3o!i#o*w=0'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition
