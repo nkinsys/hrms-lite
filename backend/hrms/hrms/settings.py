310bacb367963a620fca838468b8872fa5459030
Nishant Kumar <nishant.kumar@instantsys.com>
2026-02-28 14:39:21 +0000

    Enabled Debug Mode

--- backend/hrms/hrms/settings.py
+++ backend/hrms/hrms/settings.py
@@ -24,7 +24,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-n3=tx&*0xiepx7r-t&qdp9k6sd2qm&@e^%r+!nt+3o!i#o*w=0'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
+DEBUG = True
 
 ALLOWED_HOSTS = os.getenv("ALLOWED_HOSTS", "").split(",")
 
